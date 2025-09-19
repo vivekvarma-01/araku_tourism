e84c98f76e11a0db2fadf2fde2ac328929b1e320
Shashi Varun <137184492+Shashivarunreddy@users.noreply.github.com>
2025-09-19 08:48:10 +0530

    Update socialmedia.tsx

--- src/components/socialmedia.tsx
+++ src/components/socialmedia.tsx
@@ -6,7 +6,7 @@ export default function SocialIcons() {
     <div className="flex space-x-6">
       {/* Facebook */}
       <a
-        href="https://www.facebook.com/share/1FK15wmfJM/"
+        href=""
         target="_blank"
         rel="noopener noreferrer"
         aria-label="Facebook"
@@ -22,7 +22,7 @@ export default function SocialIcons() {
 
       {/* Instagram */}
       <a
-        href="https://www.instagram.com/araku.travels?igsh=MWNtcDJhYmxpbTF5aw=="
+        href=""
         target="_blank"
         rel="noopener noreferrer"
         aria-label="Instagram"
@@ -38,7 +38,7 @@ export default function SocialIcons() {
 
       {/* YouTube */}
       <a
-        href="https://youtube.com/@araku.travels?si=N9vP6nzaglpPBc2k"
+        href=""
         target="_blank"
         rel="noopener noreferrer"
         aria-label="YouTube"
